39b74ad1d4e4b11a58b5821760d78346e03f7f69
Deepak <deepsrh1801@gmail.com>
2021-05-20 11:59:26 +0530

    added table

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { AgGridColumn, AgGridReact } from "ag-grid-react";
+import { useEffect, useState, useRef } from "react";
+import "ag-grid-community/dist/styles/ag-grid.css";
+import "ag-grid-community/dist/styles/ag-theme-alpine.css";
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [rowData, setRowData] = useState([]);
+  const gridRef = useRef(null);
+
+  useEffect(() => {
+    fetch("https://www.ag-grid.com/example-assets/row-data.json")
+      .then((result) => result.json())
+      .then((rowData) => setRowData(rowData));
+  }, []);
+
+  // const rowData = [
+  //   { make: "Toyota", model: "Celica", price: 35000 },
+  //   { make: "Ford", model: "Mondeo", price: 32000 },
+  //   { make: "Porsche", model: "Boxter", price: 72000 },
+  // ];
+  const onButtonClick = (e) => {
+    const selectedNodes = gridRef.current.api.getSelectedNodes();
+    const selectedData = selectedNodes.map((node) => node.data);
+    const selectedDataStringPresentation = selectedData
+      .map((node) => `${node.make} ${node.model}`)
+      .join(", ");
+    alert(`Selected cars : ${selectedDataStringPresentation}`);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="ag-theme-alpine" style={{ height: 590, width: 490 }}>
+        <button onClick={onButtonClick} className="app__button">
+          Get selected rows
+        </button>
+        <AgGridReact ref={gridRef} rowData={rowData} rowSelection="multiple">
+          <AgGridColumn
+            field="make"
+            sortable={true}
+            filter={true}
+            checkboxSelection={true}
+          ></AgGridColumn>
+          <AgGridColumn
+            field="model"
+            sortable={true}
+            filter={true}
+          ></AgGridColumn>
+          <AgGridColumn
+            field="price"
+            sortable={true}
+            filter={true}
+          ></AgGridColumn>
+        </AgGridReact>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
